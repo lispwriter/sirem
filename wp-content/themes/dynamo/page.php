888a835799ff2f6682c9b81d2958b7632c18a5cf
lispwriter <lispwriter@gmail.com>
2014-02-24 23:37:51 -0800

    working it

--- wp-content/themes/dynamo/page.php
+++ wp-content/themes/dynamo/page.php
@@ -7,9 +7,7 @@
 	<div id="content" class="clearfix">
     	<div id="left-column">
         	<ul class="post-list">
-            <?php the_content(); ?>
-        		
-				<?php if (have_posts()) :
+        		<?php if (have_posts()) :
                     while (have_posts()) : the_post(); setup_postdata($post);
                         get_template_part("/functions/post-view");
                     endwhile;
@@ -18,7 +16,13 @@
                 endif; ?>
 	                
             </ul>
+            <?php if(comments_open()) {comments_template();} ?>
         </div>
+        
+        <?php if(get_option("ocmx_sidebar_layout") != "sidebarnone"): ?>
+	        <?php get_sidebar(); ?>
+	    <?php endif;?>
+        
     </div>
 	
 <?php get_footer(); ?>
